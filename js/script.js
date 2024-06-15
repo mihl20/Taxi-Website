06327269d6e12e7303d70516e129d2844ad775aa
mihl20 <mihl@hotmail.co.uk>
2024-06-15 19:00:20 +0100

    Complete main page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 function userScroll() {
     const navbar = document.querySelector('.navbar');
 
+    navbar.addEventListener('click', () => {
+        navbar.classList.add('bg-dark');
+        navbar.classList.add('p-3');
+    }
+)
+  
+document.querySelectorAll('.navbar-collapse a').forEach(function(element){
+    element.addEventListener('click', function(){
+        document.querySelector('.navbar-collapse').classList.remove('show');
+    });
+});
+
     window.addEventListener('scroll', () => {
         if (window.scrollY > 50) {
             navbar.classList.add('bg-dark');
